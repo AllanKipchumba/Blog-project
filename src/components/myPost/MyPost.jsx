d64782159f63a63784cbdfbb097f2342bba927c6
_Allan <allankipchumba76@gmail.com>
2022-06-30 19:52:35 +0300

    gets posts from db

--- src/components/myPost/MyPost.jsx
+++ src/components/myPost/MyPost.jsx
@@ -3,11 +3,11 @@ import "./myPost.scss";
 import image from "./assets/cat.jpg";
 import { Link } from "react-router-dom";
 
-const MyPost = ({ img }) => {
+const MyPost = ({ post }) => {
   return (
     <>
       <div className="post">
-        <img className="postImg" src={img} alt="" />
+        <img className="postImg" src={image} alt="" />
         <div className="postInfo">
           <div className="postCats">
             <span className="postCat">
@@ -23,18 +23,15 @@ const MyPost = ({ img }) => {
           </div>
           <span className="postTitle">
             <Link to="/post/abc" className="link">
-              Lorem ipsum dolor sit amet
+              {post.title}
             </Link>
           </span>
           <hr />
-          <span className="postDate">1 hour ago</span>
+          <span className="postDate">
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-        <p className="postDesc">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-          officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-          fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
-          atque, exercitationem quibusdam, reiciendis odio laboriosam?
-        </p>
+        <p className="postDesc">{post.description}</p>
       </div>
     </>
   );
