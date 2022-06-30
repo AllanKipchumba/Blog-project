8cac427ce4a27de2e97835e4f362ad21cdfebb2d
_Allan <allankipchumba76@gmail.com>
2022-06-30 19:44:13 +0300

    configured api proxy in package.json

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import MyPost from "../myPost/MyPost";
 import "./posts.scss";
 
-const Posts = () => {
+const Posts = ({ posts }) => {
   return (
     <>
       <div className="posts">
-        <MyPost img="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        <MyPost img="https://images.pexels.com/photos/6758029/pexels-photo-6758029.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-        <MyPost img="https://images.pexels.com/photos/6711867/pexels-photo-6711867.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        <MyPost img="https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        <MyPost img="https://images.pexels.com/photos/4916559/pexels-photo-4916559.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+        {posts.map((post) => (
+          <MyPost post={post} />
+        ))}
       </div>
     </>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
+import axios from "axios";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await axios.get("/posts");
+      // console.log(response.data);
+      setPosts(response.data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <>
       <div>
         <Header />
         <div className="home">
-          <Posts />
+          <Posts posts={posts} />
           <Sidebar />
         </div>
       </div>
