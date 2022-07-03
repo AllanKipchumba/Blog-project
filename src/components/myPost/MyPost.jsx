fc9023f4e81da42da3b84c0fe5e743aeda7796b2
_Allan <allankipchumba76@gmail.com>
2022-07-03 06:58:05 +0300

    updated SinglePost component

--- src/components/myPost/MyPost.jsx
+++ src/components/myPost/MyPost.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 import "./myPost.scss";
-import image from "./assets/cat.jpg";
 import { Link } from "react-router-dom";
 
 const MyPost = ({ post }) => {
   return (
     <>
       <div className="post">
-        <img className="postImg" src={image} alt="" />
+        {post.photo && <img className="postImg" src={post.photo} alt="" />}
         <div className="postInfo">
           <div className="postCats">
-            <span className="postCat">
-              <Link className="link" to="/posts?cat=Music">
-                Music
-              </Link>
-            </span>
-            <span className="postCat">
-              <Link className="link" to="/posts?cat=Music">
-                Life
-              </Link>
-            </span>
+            {post.categories.map((category) => {
+              <span className="postCat">
+                <Link className="link" to="/posts?cat=Music">
+                  {category.name}
+                </Link>
+              </span>;
+            })}
           </div>
           <span className="postTitle">
-            <Link to="/post/abc" className="link">
+            <Link to={`/post/${post._id}`} className="link">
               {post.title}
             </Link>
           </span>
--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./singlePost.scss";
-import image from "./assets/cat.jpg";
 import { FiEdit } from "react-icons/fi";
 import { BsTrash } from "react-icons/bs";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const SinglePost = () => {
+  const location = useLocation();
+  // access post id
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await axios.get("/posts/" + path);
+      // console.log(response.data);
+      setPost(response.data);
+    };
+    fetchPost();
+  }, [path]);
   return (
     <>
       <div className="single-post">
         <div className="wrapper">
-          <img src={image} alt="/" className="image" />
+          {post.photo && (
+            <img src={post.post.photo} alt="/" className="image" />
+          )}
           <h1>
-            Lorem ipsur dolor sit amet
+            {post.post.title}
             <div className="edit-btns">
               <FiEdit className="icon" />
               <BsTrash className="icon" />
@@ -19,34 +35,11 @@ const SinglePost = () => {
           </h1>
           <div className="post-info">
             <span>
-              Author: <span>Lanny</span>
+              Author: <span>{post.postOwner}</span>
             </span>
-            <span>1 hr ago</span>
+            <span>{new Date(post.post.createdAt).toDateString()}</span>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Non
-            tellus orci ac auctor augue mauris augue neque. Nec dui nunc mattis
-            enim ut. Eget lorem dolor sed viverra ipsum. Facilisi etiam
-            dignissim diam quis enim lobortis scelerisque fermentum. Arcu risus
-            quis varius quam quisque id diam. Ut placerat orci nulla
-            pellentesque dignissim enim. Vestibulum lorem sed risus ultricies
-            tristique nulla aliquet enim. Condimentum id venenatis a
-            condimentum. Interdum velit laoreet id donec ultrices tincidunt.
-            Tincidunt ornare massa eget egestas purus viverra accumsan in nisl.
-            Diam volutpat commodo sed egestas egestas fringilla phasellus.
-            Dictumst vestibulum rhoncus est pellentesque elit ullamcorper.
-            Viverra aliquet eget sit amet tellus cras adipiscing.
-            {""} {""} <br /> <br />
-            Nec feugiat nisl pretium fusce id velit ut tortor pretium. Quis
-            viverra nibh cras pulvinar. Volutpat est velit egestas dui id ornare
-            arcu. A lacus vestibulum sed arcu non. Leo in vitae turpis massa sed
-            elementum. Nam aliquam sem et tortor consequat id porta nibh
-            venenatis. Et netus et malesuada fames ac turpis egestas. Amet
-            cursus sit amet dictum sit amet justo donec. Faucibus a pellentesque
-            sit amet porttitor. Ac felis donec et odio pellentesque.
-            Pellentesque sit amet porttitor eget.
-          </p>
+          <p>{post.post.description}</p>
         </div>
       </div>
     </>
