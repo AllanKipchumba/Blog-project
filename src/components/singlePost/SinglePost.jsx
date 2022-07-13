7370284bcacf4fe71a197d3447acf027aa47268f
_Allan <allankipchumba76@gmail.com>
2022-07-13 08:42:27 +0300

    enabled post editing

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -17,6 +17,10 @@ const SinglePost = () => {
   const [owner, setOwner] = useState("");
   const { user } = useSelector((store) => store["loggedIn"]);
   const [tooltip, showTooltip] = useState(true);
+  //update mode
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState();
+  const [updateMode, setUpdateMode] = useState(false);
 
   // get individual posts by ID
   useEffect(() => {
@@ -37,11 +41,10 @@ const SinglePost = () => {
       // notify user of the deletion
       if (window.confirm(`Delete ${post.title}?`)) {
         // send Bearer tokens along with axios
-        axios.delete("/posts/" + path, { headers });
+        await axios.delete("/posts/" + path, { headers });
       } else {
         return false;
       }
-
       // redirect to home page
       window.location.replace("/posts");
     } catch (error) {
@@ -50,56 +53,80 @@ const SinglePost = () => {
   };
 
   // update Post
-  const updatePost = async () => {};
+  const updatePost = async () => {
+    const updates = {
+      title,
+      description,
+    };
+    try {
+      const res = await axios.patch("/posts/" + path, updates, { headers });
+      // console.log(res);
+      setUpdateMode(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <div className="single-post">
         <div className="wrapper">
           {post.photo && <img src={post.photo} alt="/" className="image" />}
-          <h1>
-            {post.title}
-            {/* render edit and delete icons if post owner is the current user in state */}
-            {owner === user?.user.username && (
-              <div className="edit-btns">
-                <FiEdit
-                  className="icon"
-                  onClick={updatePost}
-                  data-tip
-                  data-for="editPost"
-                  onMouseEnter={() => showTooltip(true)}
-                  onMouseLeave={() => {
-                    showTooltip(false);
-                    setTimeout(() => showTooltip(true), 50);
-                  }}
-                />
-                <BsTrash
-                  className="icon"
-                  onClick={deletePost}
-                  // tooltip props
-                  data-tip
-                  data-for="deletePost"
-                  onMouseEnter={() => showTooltip(true)}
-                  onMouseLeave={() => {
-                    showTooltip(false);
-                    setTimeout(() => showTooltip(true), 50);
-                  }}
-                />
-                {/* tooltip for delete */}
-                {tooltip && (
-                  <ReactTooltip id="deletePost" place="top" effect="solid">
-                    Delete post
-                  </ReactTooltip>
-                )}
-                {/* tooltip for edit */}
-                {tooltip && (
-                  <ReactTooltip id="editPost" place="top" effect="solid">
-                    Edit post
-                  </ReactTooltip>
-                )}
-              </div>
-            )}
-          </h1>
+
+          {updateMode ? (
+            <input
+              type="text"
+              placeholder={post.title}
+              value={title}
+              className="singlePostTitleInput"
+              autoFocus
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          ) : (
+            <h1>
+              {post.title}
+              {/* render edit and delete icons if post owner is the current user in state */}
+              {owner === user?.user.username && (
+                <div className="edit-btns">
+                  <FiEdit
+                    className="icon"
+                    onClick={() => setUpdateMode(true)}
+                    data-tip
+                    data-for="editPost"
+                    onMouseEnter={() => showTooltip(true)}
+                    onMouseLeave={() => {
+                      showTooltip(false);
+                      setTimeout(() => showTooltip(true), 50);
+                    }}
+                  />
+                  <BsTrash
+                    className="icon"
+                    onClick={deletePost}
+                    // tooltip props
+                    data-tip
+                    data-for="deletePost"
+                    onMouseEnter={() => showTooltip(true)}
+                    onMouseLeave={() => {
+                      showTooltip(false);
+                      setTimeout(() => showTooltip(true), 50);
+                    }}
+                  />
+                  {/* tooltip for delete */}
+                  {tooltip && (
+                    <ReactTooltip id="deletePost" place="top" effect="solid">
+                      Delete post
+                    </ReactTooltip>
+                  )}
+                  {/* tooltip for edit */}
+                  {tooltip && (
+                    <ReactTooltip id="editPost" place="top" effect="solid">
+                      Edit post
+                    </ReactTooltip>
+                  )}
+                </div>
+              )}
+            </h1>
+          )}
 
           <div className="post-info">
             <Link to={`/?author=${owner}`}>
@@ -110,7 +137,27 @@ const SinglePost = () => {
 
             <span>{new Date(post.createdAt).toDateString()}</span>
           </div>
-          <p>{post.description}</p>
+
+          {updateMode ? (
+            <>
+              <textarea
+                className="singlePostDescInput"
+                placeholder="Edit post body"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                rows="7"
+                cols="50"
+              />
+              {/* edit button */}
+              <div className="mt-10">
+                <button className="editButton" onClick={updatePost}>
+                  Update
+                </button>
+              </div>
+            </>
+          ) : (
+            <p>{post.description}</p>
+          )}
         </div>
       </div>
     </>
